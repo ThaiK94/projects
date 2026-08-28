9dcdbf540972ef8b6f5ebdebd9622651bf51bcf7
Thai Khong <thaik.dev@gmail.com>
2026-08-28 12:28:07 -0700

    bean lifecycle methods

--- demo_1/src/main/java/com/example/demo_1/common/PingPongCoach.java
+++ demo_1/src/main/java/com/example/demo_1/common/PingPongCoach.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.sql.SQLOutput;
+
 
 @Component
 public class PingPongCoach implements Coach{
