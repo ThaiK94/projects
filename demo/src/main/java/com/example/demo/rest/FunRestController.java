476c0c5c5cced2e78df8d11ef482c17a46c35645
Thai Khong <thaik.dev@gmail.com>
2026-08-27 18:04:17 -0700

    playing around with some of the properties

--- demo/src/main/java/com/example/demo/rest/FunRestController.java
+++ demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
