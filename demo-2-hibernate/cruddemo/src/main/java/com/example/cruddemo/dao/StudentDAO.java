34e22228b0653e8297ea0157030bad575407d2b3
Thai Khong <thaik.dev@gmail.com>
2026-08-29 16:59:38 -0700

    query

--- demo-2-hibernate/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAO.java
+++ demo-2-hibernate/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAO.java
@@ -2,11 +2,15 @@ package com.example.cruddemo.dao;
 
 import com.example.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findStudentByID(Integer id);
+
+    List<Student> findAll();
 
 
 
